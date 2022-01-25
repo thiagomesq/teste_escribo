d53de50e4fdea992fa54dcd4098c5e7e902c98e9
Thiago Mesquita <thiago.0306@gmail.com>
2022-01-25 10:23:50 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# teste_escribo
+
+Teste técnico dado pelo Escribo
+
+## Getting Started
+
+Para executar o projeto 
---
+++ android/app/src/main/kotlin/com/tmsistemas/teste_escribo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tmsistemas.teste_escribo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

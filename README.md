73f2a6e7dd7febfdd9d6fa51f01d641878538a1c
Thiago Mesquita <thiago.0306@gmail.com>
2022-01-25 12:03:19 -0300

    Teste completo

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Teste técnico dado pelo Escribo
 
 ## Getting Started
 
-Para executar o projeto 
+O projeto é um aplicativo de celular onde tem um campo para entrar com o valor desejado,
+
+para que seja feito o somatório, e um botão escrito Calcular, entre com o valor desejado no campo
+
+e ao apertar o botão aparecerá uma mensagem na tela contendo o valor do somatório.
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,175 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Somatorio(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Somatorio extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SomatorioState createState() => _SomatorioState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SomatorioState extends State<Somatorio> {
+  final _formKey = GlobalKey<FormState>();
+  late FocusNode _valorFocusNode;
+  late int valor;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _valorFocusNode = FocusNode();
+  }
+
+  int somatorio(int valor) {
+    int somatorio = 0;
+    for (int i = 3; i < valor; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        somatorio += i;
+      }
+    }
+    return somatorio;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Somatório'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  focusNode: _valorFocusNode,
+                  onChanged: (data) {
+                    setState(() {
+                      valor = int.parse(data);
+                    });
+                  },
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    return value!.isEmpty || int.parse(value) < 0
+                        ? 'Entre com um valor calculável.'
+                        : null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Valor',
+                    focusColor: Colors.lightBlue,
+                  ),
+                ),
+                SizedBox(height: 12.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      showGeneralDialog(
+                        barrierColor: Colors.white.withOpacity(0.5),
+                        transitionBuilder: (context, a1, a2, widget) {
+                          final curvedValue =
+                              Curves.easeInOutBack.transform(a1.value) - 1.0;
+                          return Transform(
+                            transform: Matrix4.translationValues(
+                                0.0, curvedValue * 200, 0.0),
+                            child: Opacity(
+                              opacity: a1.value,
+                              child: AlertDialog(
+                                backgroundColor: Colors.white,
+                                shape: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(16.0)),
+                                title: Text('Somatório'),
+                                content: Text(
+                                  'O resultado do somatório é ${somatorio(valor)}.',
+                                ),
+                                titleTextStyle:
+                                    const TextStyle(color: Colors.lightBlue),
+                                contentTextStyle:
+                                    const TextStyle(color: Colors.lightBlue),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    style: TextButton.styleFrom(
+                                      primary: Colors.lightBlue,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                        transitionDuration: Duration(
+                          seconds: 1,
+                        ),
+                        barrierDismissible: true,
+                        barrierLabel: '',
+                        context: context,
+                        pageBuilder: (context, a1, a2) {
+                          final curvedValue =
+                              Curves.easeInOutBack.transform(a1.value) - 1.0;
+                          return Transform(
+                            transform: Matrix4.translationValues(
+                                0.0, curvedValue * 200, 0.0),
+                            child: Opacity(
+                              opacity: a1.value,
+                              child: AlertDialog(
+                                backgroundColor: Colors.white,
+                                shape: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(16.0)),
+                                title: Text('Somatório'),
+                                content: Text(
+                                  'O resultado do somatório é ${somatorio(valor)}.',
+                                ),
+                                titleTextStyle:
+                                    const TextStyle(color: Colors.lightBlue),
+                                contentTextStyle:
+                                    const TextStyle(color: Colors.lightBlue),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    style: TextButton.styleFrom(
+                                      primary: Colors.lightBlue,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5.0,
+                    primary: Colors.lightBlue,
+                    padding: EdgeInsets.symmetric(vertical: 13.0),
+                  ),
+                  child: const Text('Calcular'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
